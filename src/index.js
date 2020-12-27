de4e3f39b95776db8507520bd8370172deee87df
Manuel Hoyo <manuel.hoyo@asesormasmovil.es>
2020-12-27 21:01:18 +0100

    Añadidos nuevos tests

--- src/index.js
+++ src/index.js
@@ -1,25 +1,85 @@
-import Producto from "./domain/producto";
-import Carrito from "./domain/carrito";
+import Producto from './domain/producto';
+import Carrito from './domain/carrito';
 
-const productoBiberon = new Producto("biberon", "nuk", 30.95);
-const productoChupete = new Producto("chupete", "nuk", 5.95);
-const productoCochecito = new Producto("cochecito", "chicco", 341.1);
+const productoBiberon = new Producto('biberon', 'nuk', 30.95);
+const productoChupete = new Producto('chupete', 'nuk', 5.95);
+const productoCochecito = new Producto('cochecito', 'chicco', 341.10);
 
 describe("Carrito", () => {
     const carrito = new Carrito();
-    // vacio este en true y el numero de items sea cero)
 
     it(`El carrito está vacío`, () => {
         expect(carrito.esVacio()).toEqual(true);
         expect(carrito.numeroItems()).toEqual(0);
     });
 
-    // test 2
     it(`La cantidad de '${productoBiberon.getDescription()}' es cero`, () => {
         expect(carrito.obtenerCantidad(productoBiberon)).toEqual(0);
     });
 
-    // test 3
+    it(`Intento añadir 0 productos '${productoChupete.getDescription()}'`, () => {
+        expect(() => {
+            carrito.anadir(productoChupete, 0);
+        }).toThrowError(Error);
+        expect(carrito.esVacio()).toEqual(true);
+        expect(carrito.numeroItems()).toEqual(0);
+        expect(carrito.obtenerCantidad(productoChupete)).toEqual(0);
+    });
+
+    it(`Intento añadir -3 productos '${productoChupete.getDescription()}'`, () => {
+        expect(() => {
+            carrito.anadir(productoChupete, -3);
+        }).toThrowError(Error);
+        expect(carrito.esVacio()).toEqual(true);
+        expect(carrito.numeroItems()).toEqual(0);
+        expect(carrito.obtenerCantidad(productoChupete)).toEqual(0);
+    });
+
+    it(`Intento añadir null productos '${productoChupete.getDescription()}'`, () => {
+        expect(() => {
+            carrito.anadir(productoChupete, null);
+        }).toThrowError(Error);
+        expect(carrito.esVacio()).toEqual(true);
+        expect(carrito.numeroItems()).toEqual(0);
+        expect(carrito.obtenerCantidad(productoChupete)).toEqual(0);
+    });
+
+    it(`Intento añadir {} productos '${productoChupete.getDescription()}'`, () => {
+        expect(() => {
+            carrito.anadir(productoChupete, {});
+        }).toThrowError(Error);
+        expect(carrito.esVacio()).toEqual(true);
+        expect(carrito.numeroItems()).toEqual(0);
+        expect(carrito.obtenerCantidad(productoChupete)).toEqual(0);
+    });
+
+    it(`Intento añadir 'notAnumber' productos '${productoChupete.getDescription()}'`, () => {
+        expect(() => {
+            carrito.anadir(productoChupete, 'notAnumber');
+        }).toThrowError(Error);
+        expect(carrito.esVacio()).toEqual(true);
+        expect(carrito.numeroItems()).toEqual(0);
+        expect(carrito.obtenerCantidad(productoChupete)).toEqual(0);
+    });
+
+    it(`Intento retirar 'notAnumber' productos '${productoChupete.getDescription()}'`, () => {
+        expect(() => {
+            carrito.retirar(productoChupete, 'notAnumber');
+        }).toThrowError(Error);
+        expect(carrito.esVacio()).toEqual(true);
+        expect(carrito.numeroItems()).toEqual(0);
+        expect(carrito.obtenerCantidad(productoChupete)).toEqual(0);
+    });
+
+    it(`Intento retirar -3 productos '${productoChupete.getDescription()}'`, () => {
+        expect(() => {
+            carrito.retirar(productoChupete, -3);
+        }).toThrowError(Error);
+        expect(carrito.esVacio()).toEqual(true);
+        expect(carrito.numeroItems()).toEqual(0);
+        expect(carrito.obtenerCantidad(productoChupete)).toEqual(0);
+    });
+
     it(`Añado un producto '${productoCochecito.getDescription()}'`, () => {
         carrito.anadir(productoCochecito, 1);
         expect(carrito.esVacio()).toEqual(false);
@@ -27,7 +87,6 @@ describe("Carrito", () => {
         expect(carrito.obtenerCantidad(productoCochecito)).toEqual(1);
     });
 
-    // test 4
     it(`Añado otro producto '${productoCochecito.getDescription()}'`, () => {
         carrito.anadir(productoCochecito, 1);
         expect(carrito.esVacio()).toEqual(false);
@@ -35,7 +94,6 @@ describe("Carrito", () => {
         expect(carrito.obtenerCantidad(productoCochecito)).toEqual(2);
     });
 
-    // test 5
     it(`Retiro un producto '${productoCochecito.getDescription()}'`, () => {
         carrito.retirar(productoCochecito, 1);
         expect(carrito.esVacio()).toEqual(false);
@@ -43,21 +101,20 @@ describe("Carrito", () => {
         expect(carrito.obtenerCantidad(productoCochecito)).toEqual(1);
     });
 
-    // test 6
-    it(`Retiro otro producto '${productoCochecito.getDescription()}'`, () => {
+    it(`Retiro un producto '${productoCochecito.getDescription()}'`, () => {
         carrito.retirar(productoCochecito);
         expect(carrito.esVacio()).toEqual(true);
         expect(carrito.numeroItems()).toEqual(0);
         expect(carrito.obtenerCantidad(productoCochecito)).toEqual(0);
     });
-    // test 7
+
     it(`Añado tres productos '${productoChupete.getDescription()}'`, () => {
         carrito.anadir(productoChupete, 3);
         expect(carrito.esVacio()).toEqual(false);
         expect(carrito.numeroItems()).toEqual(1);
         expect(carrito.obtenerCantidad(productoChupete)).toEqual(3);
     });
-    // test 8
+
     it(`Intento retirar cuatro productos '${productoChupete.getDescription()}'`, () => {
         expect(() => {
             carrito.retirar(productoChupete, 4);
@@ -66,14 +123,14 @@ describe("Carrito", () => {
         expect(carrito.numeroItems()).toEqual(1);
         expect(carrito.obtenerCantidad(productoChupete)).toEqual(3);
     });
-    // test 9
+
     it(`Vacío el carrito`, () => {
-        carrito.vaciar();
+        carrito.vaciar()
         expect(carrito.esVacio()).toEqual(true);
         expect(carrito.numeroItems()).toEqual(0);
         expect(carrito.obtenerCantidad(productoChupete)).toEqual(0);
     });
-    // test 10
+
     it(`Intento retirar un producto '${productoChupete.getDescription()}'`, () => {
         expect(() => {
             carrito.retirar(productoChupete, 1);
@@ -82,7 +139,7 @@ describe("Carrito", () => {
         expect(carrito.numeroItems()).toEqual(0);
         expect(carrito.obtenerCantidad(productoChupete)).toEqual(0);
     });
-    // test 11
+
     it(`Añado 3 '${productoChupete.getDescription()}', 2 '${productoBiberon.getDescription()}' y 1 '${productoCochecito.getDescription()}'`, () => {
         carrito.anadir(productoChupete, 3);
         carrito.anadir(productoBiberon, 2);
@@ -94,22 +151,20 @@ describe("Carrito", () => {
         expect(carrito.obtenerCantidad(productoBiberon)).toEqual(2);
         expect(carrito.obtenerCantidad(productoCochecito)).toEqual(1);
     });
-    // test 12
+
     it(`El JSON se genera correctamente`, () => {
-        expect(carrito.obtenerJson()).toEqual(
-                '[{"producto":{"nombre":"chupete","marca":"nuk","precio":5.95},"cantidad":3},{"producto":{"nombre":"biberon","marca":"nuk","precio":30.95},"cantidad":2},{"producto":{"nombre":"cochecito","marca":"chicco","precio":341.1},"cantidad":1}]'
-        );
+        expect(carrito.obtenerJson()).toEqual('[{"producto":{"nombre":"chupete","marca":"nuk","precio":5.95},"cantidad":3},{"producto":{"nombre":"biberon","marca":"nuk","precio":30.95},"cantidad":2},{"producto":{"nombre":"cochecito","marca":"chicco","precio":341.1},"cantidad":1}]');
     });
-    // test 13
+
     it(`El precio total del carrito es 420.85 (3 * 5.95 + 2 * 30.95 + 1 * 341.10)`, () => {
         expect(carrito.precioTotal()).toEqual(420.85);
     });
-    // test 14
+
     it(`El precio del carrito en productos de la marca 'nuk' es 79.75 (3 * 5.95 + 2 * 30.95)`, () => {
-        expect(carrito.precioMarca("nuk")).toEqual(79.75);
+        expect(carrito.precioMarca('nuk')).toEqual(79.75);
     });
-    // test 15
+
     it(`El precio del carrito en productos de la marca 'chicco' es 341.10 (1 * 341.10)`, () => {
-        expect(carrito.precioMarca("chicco")).toEqual(341.1);
+        expect(carrito.precioMarca('chicco')).toEqual(341.10);
     });
 });
